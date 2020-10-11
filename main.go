80ca300c1b1e5b9fb2f351c50c3324d2c8a70f82
KshitijGugale <45412660+KshitijGugale@users.noreply.github.com>
2020-10-11 18:45:11 +0530

    Add files via upload
    Added code to calculate price

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Data struct {
+	ID int
+	SKU	string
+	Name string
+	Price float32
+	Qty int
+}
+
+var Items = []Data {
+	Data {
+		ID: 1,
+		SKU: "120P90",
+		Name: "Google Home",
+		Price: 49.99,
+		Qty: 10,
+	},
+	Data {
+		ID: 2,
+		SKU: "43N23P",
+		Name: "MacBook Pro",
+		Price: 5399.99,
+		Qty: 5,
+	},
+	Data {
+		ID: 3,
+		SKU: "A304SD",
+		Name: "Alexa Speaker",
+		Price: 109.50,
+		Qty: 10,
+	},
+	Data {
+		ID: 4,
+		SKU: "234234",
+		Name: "Raspberry Pi B",
+		Price: 30,
+		Qty: 2,
+	},
+}
+func main () {
+	PrintDetails()
+
+	var id, qty int
+	fmt.Println("What you want to buy? Choose ID from 1 to 4.")
+	fmt.Scanf("%d", &id)
+	if id < 1 || id >= 5 {
+		fmt.Println("Invalid ID choosen!")
+		os.Exit(0);
+	}
+	fmt.Printf("How many items of %s you want?\n", Items[id-1].Name)
+	fmt.Scanf("%d", &qty)
+	CalculatePrice(id, qty)
+
+}
+
+func CalculatePrice(id, qty int) {
+	var totalPrice float32
+	if id == 1 {
+		totalPrice = totalPrice + Items[id-1].Price * float32(qty)
+	} else if id == 2 {
+		var div int = qty / 3
+		var rem int = qty % 3
+
+		totalPrice += Items[id-1].Price * float32(div) * 2
+		totalPrice += Items[id-1].Price * float32(rem)
+	} else if id == 3 {
+		totalPrice = totalPrice + Items[id-1].Price * float32(qty)
+		if qty > 3 {
+			totalPrice -= (0.1) * totalPrice
+		}
+	} else if id == 4 {
+		totalPrice = totalPrice + Items[id-1].Price * float32(qty)
+	}
+
+	fmt.Printf("Total Price: %0.2f\n", totalPrice)
+}
+
+func PrintDetails() {
+	for _, value := range Items {
+		fmt.Printf("ID: %d, SKU: %s, Name: %s, Price: %f, Qty: %d\n", value.ID, value.SKU, value.Name, value.Price, value.Qty)
+	}
+}
